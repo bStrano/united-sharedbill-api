11440b305f91ff37aaefe8eabf9a9bd1c282527a
Bruno Strano <bruno_strano@hotmail.com>
2023-08-30 23:05:44 -0300

    🔧 Swagger config

---
+++ src/config/swagger/swagger.config.ts
@@ -0,0 +1,15 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageJson from '../../../package.json';
+import { INestApplication } from '@nestjs/common';
+
+export const initializeSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('United API')
+    .setDescription('United API Description')
+    .setVersion(packageJson.version)
+    .addTag('nestjs')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
